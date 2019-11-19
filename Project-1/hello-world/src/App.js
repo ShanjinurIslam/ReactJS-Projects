c8df439aa7935f216818803fb617debe7c47fe5a
Shanjinur Islam <spondoncsebuet@gmail.com>
2019-11-19 23:20:52 +0600

    First Project Added

---
+++ Project-1/hello-world/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Hello World</code>
+        </p>
+        <p>from <code>shanjinur</code></p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
