7fec7733f3d06cfb38229c7ad0700ff1859b2c14
Shanjinur Islam <spondoncsebuet@gmail.com>
2019-11-19 23:39:04 +0600

    Added first component

--- Project-1/hello-world/src/App.js
+++ Project-1/hello-world/src/App.js
@@ -5,16 +5,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello World</code>
-        </p>
-        <p>from <code>shanjinur</code></p>
-
-      </header>
+      <Header/>
     </div>
   );
+}
+
+class Header extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1 className="App-link">Inside Header class</h1>
+        <Intro />
+      </div>
+    );
+  }
+}
+
+class Intro extends React.Component {
+  render(){
+    return(
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>Inside Intro Class</code>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
